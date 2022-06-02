9fdebfdb177108fd374636f664a4c686cc118fa9
Marisame <rulivas26@gmail.com>
2022-06-02 10:41:39 -0500

    Routes Finish!!

---
+++ pkg/routes/pkmstore-routers.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/Marisame254/PkmRestApi/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterPkmRoutes = func(router *mux.Router) {
+	router.HandleFunc("/pokemon", controllers.CreatePkm).Methods("POST")
+	router.HandleFunc("/pokemon", controllers.GetPkm).Methods("GET")
+	router.HandleFunc("/pokemon/{Id}", controllers.GetPkmById).Methods("GET")
+	router.HandleFunc("/pokemon/{Id}", controllers.UpdatePkm).Methods("PUT")
+	router.HandleFunc("/pokemon/{Id}", controllers.DeletePkm).Methods("DELETE")
+}
